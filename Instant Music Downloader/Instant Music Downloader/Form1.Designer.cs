4482c16b3a483101173cd32b4a51f44b0e48dc21
yashbansal24 <yashbansal24@users.noreply.github.com>
2016-06-13 23:24:57 +0530

    Initial Commit

---
+++ Instant Music Downloader/Instant Music Downloader/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Instant_Music_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 224);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 272);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(438, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 365);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Instant Music Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Instant Music Downloader/Instant Music Downloader/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Instant_Music_Downloader
+{
+    public partial class Form1 : Form
+    {
+        static int count = 0;
+        WebClient client, webclient;
+        String dest;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dest = fbd.SelectedPath;
+            }
+            int x = 0;
+            string[] sep = new string[] { "\r\n" };
+            string[] lines = textBox1.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++)
+            {
+
+                x = download(lines[i]);
+
+
+            }
+            // label1.Text = "Downloaded all songs";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private int download(string song)
+        {
+            label1.Text = "Starting Download ... Please Wait ...";
+            string name = song;
+            song = query(song);
+            using (webclient = new WebClient())
+            {
+                string reply = webclient.DownloadString("https://www.youtube.com/results?search_query=" + song);
+                string download_url = Embed(reply);
+                using (client = new WebClient())
+                {
+                    client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                    client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                    client.DownloadFileAsync(new Uri("https://www.youtubeinmp3.com/fetch/?video=" + download_url), @dest + "\\" + name + ".mp3");
+                }
+            }
+
+            return 0;
+
+
+
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            count++;
+            label1.Text = "Downloaded " + count.ToString() + " song(s)";
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = "Downloading";
+        }
+        private string query(string s)
+        {
+            string[] words = s.Split(' ');
+            string re = "";
+            foreach (string word in words)
+            {
+                re += word + "+";
+            }
+            return re.Substring(0, re.Length - 1);
+        }
+
+        private String Embed(string s)
+        {
+            int n = s.Length;
+            int index = s.IndexOf("/watch?v=");
+            index = index + "/watch?v=".Length;
+            string re = "https://www.youtube.com/watch?v=";
+
+            for (int i = index; i < n; i++)
+            {
+                if (s[i] == '"') break;
+
+                else re += "" + s[i];
+            }
+
+            return re;
+
+        }
+
+        
+
+        
+    }
+}
